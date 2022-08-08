3c9343bbee206c7d6dbae5c188205c705f10bb7d
harunabdulmajid <harunaabdulmajid7@gmail.com>
2022-08-08 05:31:09 -0700

    m

--- main.h
+++ main.h
@@ -5,5 +5,5 @@ int _printf(const char *format, ...);
 int _putchar(char c);
 char *convert(unsigned long int num, int base, int lowercase);
 int _puts(char *str);
-
+int (*get_print(char s))(va_list, flags_t *);
 #endif
