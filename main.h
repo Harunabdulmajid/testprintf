3a9eab30b2222afd42760d09a30712fa864ed27a
harunabdulmajid <harunaabdulmajid7@gmail.com>
2022-08-08 07:54:52 -0700

    changes made

--- main.h
+++ main.h
@@ -1,9 +1,28 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int _printf(const char *format, ...);
+#include <stdio.h>
+#include <stdarg.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/**
+ * struct types - specifier structure for printf
+ * @p: pointer to character specifier
+ * @func: function pointer to point function
+ */
+typedef struct types
+{
+	char p;
+	int (*func)(va_list);
+} print_f;
+
 int _putchar(char c);
-char *convert(unsigned long int num, int base, int lowercase);
-int _puts(char *str);
-int (*get_print(char s))(va_list, flags_t *);
+int _printf(const char *format, ...);
+int printc(va_list l);
+int print_string(va_list s);
+int (*cmp_func(const char a))(va_list);
+int print_n(va_list n);
+
 #endif
